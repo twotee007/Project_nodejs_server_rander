a3ad207b7df71cc6e938c931a140597fd4b9b422
twotee007 <rexllyz2003@gmail.com>
2024-03-01 22:25:59 +0700

    update app

--- app.ts
+++ app.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import { router as login } from "./api/login";
 import bodyParser from "body-parser";
+import cors from "cors";
 export const app = express();
-
+app.use(
+    cors({
+      origin: "*",
+    })
+  );
 app.use(bodyParser.text());
 app.use(bodyParser.json());
 
