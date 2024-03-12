7f554a5b2911c0f54aefbf37febd893c5221ae0d
twotee007 <rexllyz2003@gmail.com>
2024-03-12 10:29:33 +0700

    update

--- api/rank.ts
+++ api/rank.ts
@@ -38,17 +38,17 @@ router.get("/today", (req, res) => {
   conn.query(
     `
         SELECT  
-            images.*,
-            RANK() OVER (ORDER BY SUM(vote.score) DESC) AS rankingtoday
-        FROM 
-            images,vote
-        WHERE  
-            images.imgid = vote.imgid
-        GROUP BY 
-            images.imgid, images.imgurl, images.name, images.score, images.uid
-        ORDER BY 
-            images.score DESC
-        LIMIT 0, 10 `,
+        images.*,
+        ROW_NUMBER() OVER (ORDER BY SUM(vote.score) DESC) AS rankingtoday
+    FROM 
+        images
+    INNER JOIN 
+        vote ON images.imgid = vote.imgid
+    GROUP BY 
+        images.imgid, images.imgurl, images.name, images.score, images.uid
+    ORDER BY 
+        images.score DESC
+    LIMIT 0, 10; `,
     (err, result, fields) => {
       if (result && result.length > 0) {
         // ส่ง response กลับด้วยข้อมูลผู้ใช้
