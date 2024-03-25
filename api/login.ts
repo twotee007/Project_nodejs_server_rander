fa0867011f70c16a0cf46993cc07c8715dc82171
twotee007 <rexllyz2003@gmail.com>
2024-03-25 11:42:56 +0700

    update

--- api/login.ts
+++ api/login.ts
@@ -7,16 +7,19 @@ export const router = express.Router();
 
 
 router.get("/", (req, res) => {
-    conn.query('select * from user', (err, result, fields)=>{
-        if (result && result.length > 0) {
-            // ส่ง response กลับด้วยข้อมูลผู้ใช้
-            res.json(result);
-        } else {
-            // ถ้าไม่พบผู้ใช้, ส่ง response กลับเป็น { success: false }
-            res.json({
-                success: false,
-            });
-        }
+    let typeuser = 'user';
+    const sql ="SELECT * FROM user WHERE type = ?";
+    conn.query(sql, [typeuser], (err, result, fields) => {
+            // ตรวจสอบว่ามีผลลัพธ์หรือไม่
+            if (result && result.length > 0) {
+                // ส่ง response กลับด้วยข้อมูลผู้ใช้
+                res.json(result);
+            } else {
+                // ถ้าไม่พบผู้ใช้, ส่ง response กลับเป็น { success: false }
+                res.json({
+                    success: false,
+                });
+            }
     });
   });
 
